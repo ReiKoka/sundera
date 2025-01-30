d7c4c1cea4a403a24e36fb841c191b1000e2e77e
ReiKoka <reikoka1@outlook.com>
2025-01-30 12:50:26 +0100

    Added icons on buttons for reset and adding filters

--- src/js/renderFilters.js
+++ src/js/renderFilters.js
@@ -53,13 +53,23 @@ export const renderFilters = (products) => {
         </div>
       </div>
       
-      <button type="button" class="btn reset-filters-btn">Reset</button>
-      <button type="submit" class="btn apply-filters-btn">Apply Filters</button>
+      <button type="button" class="btn reset-filters-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+        <span>Reset</span>
+      </button>
+      <button type="submit" class="btn apply-filters-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+          <path d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM12.75 12a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM9 15.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+        </svg>
+        <span>Apply Filters</span>
+      </button>
     </form>
   `;
 
   productsMain.insertBefore(utilitiesContainer, productsContainer);
-  
+
   const form = document.querySelector("#filters-form");
   const resetBtn = document.querySelector(".reset-filters-btn");
   const categorySelect = document.querySelector("#categories");
@@ -96,6 +106,4 @@ export const renderFilters = (products) => {
       })
       .catch((err) => console.error(err));
   });
-
-
 };
